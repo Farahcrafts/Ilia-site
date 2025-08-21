0dd367b5cfcb5b02e8824ed75d0833657e6ad639
Farah <hamlilifarah.2005@gmail.com>
2025-08-21 17:27:08 +0100

    assembler les pages

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ let submitBtn = document.getElementById("submit");
 
 submitBtn.onclick = function () {
   let username = document.getElementById("nom-prenom").value;
-  username = username.trim();
-  console.log(username);
-  username = username.slice(username.indexOf(" "), username.length);
-  console.log(username);
-  document.getElementById(
-    "submit-message"
-  ).textContent = `Vos informations ont été soumises, nous vous contacterons bientôt, ${username}! `;
+  if (username === "") {
+    document.getElementById(
+      "submit-message"
+    ).textContent = `Veuillez remplir le formulaire!`;
+  } else {
+    username = username.trim();
+    console.log(username);
+    username = username.slice(username.indexOf(" "), username.length);
+    console.log(username);
+    document.getElementById(
+      "submit-message"
+    ).textContent = `Vos informations ont été soumises, nous vous contacterons bientôt, ${username}! `;
+  }
 };
